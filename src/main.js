86296c249549f6624ffee27346f9ef60ff3c4959
Dima <zahrebelny@gmail.com>
2020-11-22 23:48:58 +0200

    added header elements

--- src/main.js
+++ src/main.js
@@ -1,1 +1,21 @@
+import {createTripInfo} from "./view/trip-info";
+import {createTravelCost} from './view/travel-cost';
+import {createMenu} from './view/menu';
+import {createFilters} from './view/filters';
 
+const render = (container, template, place) => {
+  container.insertAdjacentHTML(place, template);
+};
+
+const siteMainElement = document.querySelector(`.page-body`);
+const siteHeaderElement = siteMainElement.querySelector(`.page-header`);
+const headerTripMain = siteHeaderElement.querySelector(`.trip-main`);
+const headerMenu = headerTripMain.querySelector(`.trip-controls`);
+
+render(headerTripMain, createTripInfo(), `afterbegin`);
+
+const headerTripInfo = headerTripMain.querySelector(`.trip-info`);
+
+render(headerTripInfo, createTravelCost(), `beforeend`);
+render(headerMenu, createMenu(), `afterbegin`);
+render(headerMenu, createFilters(), `beforeend`);
