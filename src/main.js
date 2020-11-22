b330cd901ad17d8b2139159cc4835676af72fe41
Dima <zahrebelny@gmail.com>
2020-11-23 00:58:20 +0200

    added main content

--- src/main.js
+++ src/main.js
@@ -2,15 +2,20 @@ import {createTripInfo} from "./view/trip-info";
 import {createTravelCost} from './view/travel-cost';
 import {createMenu} from './view/menu';
 import {createFilters} from './view/filters';
+import {createSort} from './view/sort';
+import {createTripEventsList} from './view/trip-events-list';
+import {createCreationForm} from './view/creation-form';
 
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
 };
 
-const siteMainElement = document.querySelector(`.page-body`);
-const siteHeaderElement = siteMainElement.querySelector(`.page-header`);
+const siteBodyElement = document.querySelector(`.page-body`);
+const siteHeaderElement = siteBodyElement.querySelector(`.page-header`);
+const siteMainElement = siteBodyElement.querySelector(`.page-main`);
 const headerTripMain = siteHeaderElement.querySelector(`.trip-main`);
 const headerMenu = headerTripMain.querySelector(`.trip-controls`);
+const tripEvent = siteMainElement.querySelector(`.trip-events`);
 
 render(headerTripMain, createTripInfo(), `afterbegin`);
 
@@ -19,3 +24,9 @@ const headerTripInfo = headerTripMain.querySelector(`.trip-info`);
 render(headerTripInfo, createTravelCost(), `beforeend`);
 render(headerMenu, createMenu(), `afterbegin`);
 render(headerMenu, createFilters(), `beforeend`);
+render(tripEvent, createSort(), `afterbegin`);
+render(tripEvent, createTripEventsList(), `beforeend`);
+
+const tripList = tripEvent.querySelector(`.trip-events__list`);
+
+render(tripList, createCreationForm(), `afterbegin`);
