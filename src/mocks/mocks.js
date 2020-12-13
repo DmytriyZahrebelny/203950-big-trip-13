92152a32372ed46a129e474f7d845a7e97dde293
Dima <zahrebelny@gmail.com>
2020-12-13 13:46:40 +0200

    added mocks for points

---
+++ src/mocks/mocks.js
@@ -0,0 +1,18 @@
+const getRandomInteger = (a = 0, b = 1) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+
+  return Math.floor(lower + Math.random() * (upper - lower + 1));
+};
+
+const pointTypes = [`Taxi`, `Bus`, `Train`, `Ship`, `Transport`, `Drive`, `Flight`, `Check-in`, `Sightseeing`];
+
+const generateTypePoint = () => pointTypes[getRandomInteger(0, pointTypes.length - 1)];
+
+export const generatePoint = () => ({
+  type: generateTypePoint(),
+  time: ``,
+  isFavorite: Boolean(getRandomInteger()),
+  destination: `London`,
+  price: 400
+});
