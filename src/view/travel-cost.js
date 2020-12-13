afff74a85ed20942493583af1df3718250550f00
Dima <zahrebelny@gmail.com>
2020-12-13 22:02:29 +0200

    added toggle on points

--- src/view/travel-cost.js
+++ src/view/travel-cost.js
@@ -1,5 +1,29 @@
-export const createTravelCost = () => (`
+import {createElement} from "../utils";
+
+const createTravelCost = () => (`
   <p class="trip-info__cost">
     Total: &euro;&nbsp;<span class="trip-info__cost-value">1230</span>
   </p>
 `);
+
+export default class TravelCost {
+  constructor() {
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createTravelCost();
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
--- src/view/trip-events-list.js
+++ src/view/trip-events-list.js
@@ -1,3 +1,27 @@
-export const createTripEventsList = () => (`
+import {createElement} from "../utils";
+
+const createTripEventsList = () => (`
   <ul class="trip-events__list"></ul>
 `);
+
+export default class TripEventsList {
+  constructor() {
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createTripEventsList();
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
